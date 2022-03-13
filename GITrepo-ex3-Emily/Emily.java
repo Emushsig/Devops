08fcff085da94ec71f57b9e9e4841a92f7f4c1c2
“Emily <emush.sigal@gmail.com>
2022-03-13 23:59:32 +0200

    Emily changed

--- GITrepo-ex3-Emily/Emily.java
+++ GITrepo-ex3-Emily/Emily.java
@@ -1,5 +1,7 @@
 public class Emily{
-	for (int i=0;i<10;i++){
-		System.out.println("Emilly "+i);
-	}
+public static void main(String[] args) {
+for (int i=1;i<=15; i++) {
+System.out.println("Emily"+i);
+}
+}
 }
